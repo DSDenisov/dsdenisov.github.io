6810d9a02d1e8567177b620eb6be9f31bfe81a7c
DmitryPro <dsdenisov@gmail.com>
2018-02-13 23:49:43 -0800

    gradiend task done

--- JS/JS_Grad/script.js
+++ JS/JS_Grad/script.js
@@ -2,6 +2,12 @@ var css = document.querySelector("h3");
 var color1 = document.querySelector(".color1");
 var color2 = document.querySelector(".color2");
 var body = document.getElementById("gradient");
+var click = document.getElementById("spin");
+
+chGradient();
+css.textContent = body.style.background+";"
+
+
 
 function chGradient() {
 	body.style.background = 
@@ -10,7 +16,19 @@ function chGradient() {
 	+", "
 	+color2.value
 	+")";
+
+	css.textContent = body.style.background+";"
 }
 
 color1.addEventListener("input", chGradient);
 color2.addEventListener("input", chGradient);
+click.addEventListener("click", function(){
+	var first = '#' + Math.random().toString(16).slice(2, 8).toUpperCase();
+	var second = '#' + Math.random().toString(16).slice(2, 8).toUpperCase();
+
+	color1.value = first;
+	color2.value = second;
+
+	chGradient();
+});
+
